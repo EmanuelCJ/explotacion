37b4f2ed6b52fc35fc3ca537910083acdcdcba2f
EmanuelCJ <emanuelcastrillo95@gmail.com>
2026-03-25 13:53:53 -0300

    agregamos migracion de datos de csv

---
+++ backend/data/migracion_datos_csv.py
@@ -0,0 +1,12 @@
+# from app.db.conexion_DB import ConectDB
+import pandas as pd
+
+info = pd.read_csv('data/materiales_categorizados.csv')
+
+
+for index, row in info.iterrows():
+    id_material = row.iloc[0]
+    nombre = row.iloc[1]
+    categoria = row.iloc[2]
+    
+    print(f"ID: {id_material}, Nombre: {nombre}, Categoria: {categoria}")
