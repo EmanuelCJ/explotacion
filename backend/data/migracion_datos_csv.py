143dae579544a34b83b3fa3fe99ff105fa14dbf5
EmanuelCJ <emanuelcastrillo95@gmail.com>
2026-03-26 14:10:41 -0300

    seguir con cambios migracion

--- backend/data/migracion_datos_csv.py
+++ backend/data/migracion_datos_csv.py
@@ -1,14 +1,22 @@
 # from app.db.conexion_DB import ConectDB
 import pandas as pd
+import re
 
 info = pd.read_csv('data/materiales_categorizados.csv')
 
+
+def asignar_unidad(categoria):
+    categoria = str(categoria).upper()
+
+    if "Tuberias PVC" in categoria:
+        return "Metros"
+    else:
+        return "Unidades"
 
 for index, row in info.iterrows():
     id_material = row.iloc[0]
     nombre = row.iloc[1]
     categoria = row.iloc[2]
-
+    unidad_medida = asignar_unidad(categoria)
     
-    
-    print(f"ID: {id_material}, Nombre: {nombre}, Categoria: {categoria}")
+    print(f"ID: {id_material}, Nombre: {nombre}, Categoría: {categoria}, Unidad de Medida: {unidad_medida}")
