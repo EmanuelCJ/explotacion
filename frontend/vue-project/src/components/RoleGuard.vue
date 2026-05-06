268be89b714e965a9073cead96de82dc8221652b
EmanuelCJ <emanuelcastrillo95@gmail.com>
2026-05-06 07:06:14 -0300

    comienzo de agregado de seguridad UX

---
+++ frontend/vue-project/src/components/RoleGuard.vue
@@ -0,0 +1,14 @@
+<!-- RoleGuard.vue -->
+<template>
+  <slot v-if="allowed" />
+</template>
+
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore'
+
+const props = defineProps<{ roles: string[] }>()
+
+const auth = useAuthStore()
+
+const allowed = auth.hasRole(props.roles)
+</script>
